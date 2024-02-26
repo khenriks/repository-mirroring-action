45549af089a2b9f818b03115ce7b07fb0ce30dc7
K Henriksson <khenriksson@bgsh.org>
2024-02-25 17:10:44 -0800

    Remove SSH username parameter
    It's redundant and annoying.

--- mirror.sh
+++ mirror.sh
@@ -3,7 +3,7 @@ set -eu
 
 /setup-ssh.sh
 
-export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -l $INPUT_SSH_USERNAME"
+export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no"
 git remote add mirror "$INPUT_TARGET_REPO_URL"
 git push --tags --force --prune mirror "refs/remotes/origin/*:refs/heads/*"
 
